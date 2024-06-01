23b7ab466ce16879ee943ad15f212e8843e7798e
Sheedh Mashood <sheedh.mashood@gmail.com>
2024-06-01 21:38:51 +0530

    updated about us page

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -97,9 +97,8 @@
                             <div class="hover-content">
                                 <ul class="social-icons">
                                     <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                                     <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -107,7 +106,7 @@
                     <div class="down-content">
                         <h4>Sheedh Mashood</h4>
                         <span>Developer</span>
-                        <p>An ambitious undergraduate at APIIT Colombo, Sri Lanka, specializing in software development and passionate about creating innovative tech solutions</p>
+                        <p>An ambitious undergraduate at APIIT Colombo, Sri Lanka, specializing in software development.</p>
                     </div>
                 </div>
             </div>
